fecc3d27729e9bfff492b41eaa47a951b4b637ab
tetsuo <ogunduz@gmail.com>
2014-08-31 07:17:46 +0300

    pass options.minify to minify before compile

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ var Hogan = require('hogan.js');
 var fs = require('co-fs');
 var read = fs.readFile;
 var extensions = /(hogan|hg|mustache|ms)$/;
+var extend = require('util')._extend;
+var minify = require('html-minifier').minify;
 
 module.exports = hogan;
 
+var minifierDefaults = {
+    removeComments               : true,
+    collapseWhitespace           : true,
+    removeAttributeQuotes        : false,
+    removeCommentsFromCDATA      : false,
+    removeCDATASectionsFromCDATA : false,
+    collapseBooleanAttributes    : false,
+    removeRedundantAttributes    : false,
+    useShortDoctype              : false,
+    removeOptionalTags           : false,
+    removeEmptyElements          : false
+};
+
 function hogan (options) {
-  options || (options = { asString: true });
+  options || (options = {});
+  options = extend(options, {asString: true});
+
   var f = true;
-  
   return function *hogan (file, entry) {
     if (!extensions.test(file.type) || 'js' != entry.type)
       return;
@@ -21,6 +37,7 @@ function hogan (options) {
       f = false;
     }
 
+    if (options.minify) file.src = minify(file.src, minifierDefaults);
     var cobj = Hogan.compile(file.src, options);
     file.src = 'var Template = require(\'hogan-runtime\').Template;' + 
       'module.exports = new Template(' + cobj + ');';
